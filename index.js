cbac79e2020af9a75d8a1b932bdb5fe74750d52a
Jenni Rivera <jenninrivera@gmail.com>
2023-08-02 10:35:52 -0400

    another solution added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById('text').textContent = "This is really cool!"
-})
+// document.addEventListener("DOMContentLoaded", function() {
+//     document.getElementById('text').textContent = "This is really cool!"
+// })
 
+//The below code uses an arrow function to make the code more consise
 
+document.addEventListener("DOMContentLoaded", e => document.getElementById('text').textContent = "This is really cool!")
